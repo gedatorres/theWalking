abec954af45e7db0fddb5d5bce830c04db48b676
gedatorres <daniel.torres5654l@gmail.com>
2025-02-17 01:47:43 -0300

    Agregar contenido de la página web

---
+++ static/js/scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+    links.forEach(link => {
+        link.addEventListener("click", function() {
+            links.forEach(link => link.classList.remove("active"));
+            this.classList.add("active");
+        });
+    });
+});
